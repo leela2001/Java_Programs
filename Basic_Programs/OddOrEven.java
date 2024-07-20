33c560977f41fab001e8361b62fcb80fbe3c132f
LEELA D <88545963+leela2001@users.noreply.github.com>
2024-07-20 18:57:16 +0530

    Create OddOrEven.java

---
+++ Basic_Programs/OddOrEven.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class OddOREven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number ;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number : ");
+		number = sc.nextInt();
+		
+//		1 st method
+		
+		if(number % 2 == 0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+		
+//		2nd method
+ 		
+		String oddOrEven = number % 2 == 0 ? "Even" : "Odd" ;
+		System.out.println(oddOrEven);
+		
+	}
+	
+
+}
